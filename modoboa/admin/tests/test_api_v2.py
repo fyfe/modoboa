f4a438e6ac8442a25431a9744b1ec8bcc8e866a6
Andrew Fyfe <andrew@neptune-one.net>
2018-04-16 22:29:16 +0100

    f

--- modoboa/admin/tests/test_api_v2.py
+++ modoboa/admin/tests/test_api_v2.py
@@ -8,12 +8,14 @@ from pprint import pprint
 
 from django.urls import reverse
 
-from modoboa.admin import factories
+from modoboa.admin import factories, models
 from modoboa.lib.tests import ModoAPITestCase
 
 
 class DomainAPITestCase(ModoAPITestCase):
     """Check API."""
+
+    domain_list_url = reverse("api_v2:domain-list")
 
     @classmethod
     def setUpTestData(cls):  # NOQA:N802
@@ -23,10 +25,30 @@ class DomainAPITestCase(ModoAPITestCase):
 
     def test_get_domains(self):
         """Retrieve a list of domains."""
-        url = reverse("api_v2:domain-list")
-        response = self.client.get(url)
+        response = self.client.get(self.domain_list_url)
         pprint(response.data)
         pprint(response)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 2)
         pprint(response.data)
+
+    def test_create(self):
+        data = {
+            "name": "example.com",
+        }
+        response = self.client.post(self.domain_list_url, data)
+        pprint(response)
+
+    def test_create_idn_as_ascii(self):
+        data = {
+            "name": "xn--fsq.com",  # 例.com; 例 == example in Japanese
+        }
+        response = self.client.post(self.domain_list_url, data)
+        pprint(response)
+
+    def test_create_idn_as_unicode(self):
+        data = {
+            "name": "例.com",  # xn--fsq.com; 例 == example in Japanese
+        }
+        response = self.client.post(self.domain_list_url, data)
+        pprint(response)
