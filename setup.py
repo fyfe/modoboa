c62f579de752bbe7b654061fccbed31ad88400a6
Andrew Fyfe <andrew@neptune-one.net>
2018-04-16 22:05:44 +0100

    DO NOT MERGE isort

--- setup.py
+++ setup.py
@@ -13,13 +13,14 @@ from __future__ import unicode_literals
 import io
 from os import path
 
+from setuptools import find_packages, setup
+
 try:
     from pip.req import parse_requirements
 except ImportError:
     # pip >= 10
     from pip._internal.req import parse_requirements
 
-from setuptools import find_packages, setup
 
 
 def get_requirements(requirements_file):
