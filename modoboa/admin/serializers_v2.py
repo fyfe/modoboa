6a5071dfe179e52007c2bcbaeb8fa9fde07e5c1c
Andrew Fyfe <andrew@neptune-one.net>
2018-04-17 00:05:19 +0100

    f

--- modoboa/admin/serializers_v2.py
+++ modoboa/admin/serializers_v2.py
@@ -7,10 +7,13 @@ from __future__ import unicode_literals
 from pprint import pprint
 
 from rest_framework import serializers
+from modoboa.parameters import tools as param_tools
 
 from modoboa.admin import models as admin_models
 from modoboa.lib.idn import convert_idn
 from modoboa.transport.serializers_v2 import TransportSerializer
+from django.utils.translation import ugettext as _
+from modoboa.admin.lib import domain_has_authorized_mx
 
 
 class DomainSerializer(serializers.ModelSerializer):
@@ -21,14 +24,14 @@ class DomainSerializer(serializers.ModelSerializer):
     class Meta:
         model = admin_models.Domain
         fields = "__all__"
+        read_only_fields = ("id", "creation", "last_modification")
 
     def to_representation(self, obj):
         rep = super(DomainSerializer, self).to_representation(obj)
         if self.context.get("export", False):
             # don't export auto-generated fields
-            del rep["id"]
-            del rep["creation"]
-            del rep["last_modification"]
+            for field in self.meta.read_only_fields:
+                del rep[field]
         else:
             rep["tags"] = obj.tags
             rep["dnsbl_status_color"] = obj.dnsbl_status_color
@@ -57,6 +60,41 @@ class DomainSerializer(serializers.ModelSerializer):
                 value["name"], self.context.get("idn_as_ascii", False)
             )
         return value
+
+    def validate(self, data):
+        if data.get("enable_dns_checks", True):
+            self._validate_mx(data)
+        self._validate_quota(data)
+        return data
+
+    def _validate_mx(self, data):
+        """Check name constraints."""
+        is_superuser = self.context["request"].user.is_superuser
+        domains_must_have_authorized_mx = (
+            param_tools.get_global_parameter("domains_must_have_authorized_mx")
+        )
+        domain_mx_authorized = (
+            is_superuser or (
+                domains_must_have_authorized_mx and
+                domain_has_authorized_mx(data["name"])
+            )
+        )
+        if not domain_mx_authorized:
+            raise serializers.ValidationError(
+                _("No authorized MX record found for this domain")
+            )
+        return data
+
+    def _validate_quota(self, data):
+        """Check quota values."""
+        quota = data.get("quota", 0)
+        default_mailbox_quota = data.get("default_mailbox_quota", 0)
+        if quota != 0 and default_mailbox_quota > quota:
+            raise serializers.ValidationError({
+                "default_mailbox_quota":
+                _("Cannot be greater than domain quota")
+            })
+        return data
 
 
 class DomainAliasSerializer(serializers.ModelSerializer):
